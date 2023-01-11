a96676b4eb46c5d412c1671f56806c26d3383923
Enrique Iturriaga <enriqueiturriagaa@gmail.com>
2023-01-11 12:58:50 -0400

    Fixed About Page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,22 +1,61 @@
 import React from 'react'
+import { groq } from "next-sanity";
+import { client } from "../lib/sanity.client";
 import AboutComp from '../components/AboutComp';
 import Footer from '../components/Footer';
 import Header from '../components/Header'
 import RightPannel from '../components/RightPannel';
 import '../styles/globals.css';
+import Popular from '../components/Popular';
+import Services from '../components/Services';
 
 
-function about() {
+const query2 = groq`
+    *[_type == "category"]{
+  _id,
+  _type,
+  title,
+  slug,
+  "posts": *[_type == "post" && references(^._id)]{
+    title,
+    slug
+  }
+}`
+export const revalidate = 60;
+export default function about() {
+
+    // const categories = await client.fetch(query2);
     return (
         <div className='bg-[#FBFAFB] '>
             <div className='max-w-5xl mx-auto'>
                 <Header />
                 <div className="bg-[#FBFAFB] text-[#343434] flex  px-12">
-                    <div className="w-full lg:w-2/3  lg:pr-20 min-w-[60%]">
+                    <div className="w-full lg:w-2/3 auto lg:pr-20 min-w-[60%]">
                         <AboutComp />
                     </div>
                     <div className="font-gochi lg:1/3 hidden lg:inline max-w-md">
-                        <RightPannel />
+                        {/* <RightPannel categories={categories} /> */}
+                        <div className='sticky top-10'>
+
+                            <div className='mb-12'>
+                                <h2 className='text-xl mb-2'>Things I can do:</h2>
+                                <div className=''>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I code</p>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I design</p>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I Grapple</p>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I Surf</p>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I cook great picanha</p>
+                                    <p className='inline-block text-left bg-[#FFEBE0] px-3 py-1 mr-2 text-sm font-gochi mb-2 '>I build products</p>
+
+
+                                </div>
+                            </div>
+                            {/* ENDS */}
+                            <Popular />
+                            <Services />
+
+                        </div>
+
                     </div>
 
                 </div>
@@ -26,5 +65,3 @@ function about() {
         </div>
     )
 }
-
-export default about
